d4352f7ef335508a7f7d78fdf3035d65837332e6
Proxtx <39201896+Proxtx@users.noreply.github.com>
2023-08-28 00:19:34 +0200

    try catch

--- static/main.js
+++ static/main.js
@@ -2,8 +2,9 @@ const imageApi = await framework.load("image.js");
 const img = document.getElementById("img");
 
 const imgLoop = async () => {
-  img.src = "data:image/png;base64, " + (await imageApi.getImage());
-
+  try {
+    img.src = "data:image/png;base64, " + (await imageApi.getImage());
+  } catch {}
   setTimeout(() => imgLoop(), 1000);
 };
 
